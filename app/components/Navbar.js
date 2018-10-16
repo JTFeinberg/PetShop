8aeb430c5480cd7d373ea21fc29494f642324d49
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-16 10:34:25 -0400

    Create Navbar component

---
+++ app/components/Navbar.js
@@ -0,0 +1,42 @@
+"use strict";
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchCentersThunk} from '../store'
+import {NavLink} from 'react-router-dom'
+
+
+class Navbar extends Component {
+    componentDidMount() {
+        this.props.loadAllCenters()
+    }
+
+    render() {
+        return (
+          <nav>
+            <NavLink to="/" >
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE5sMxBv77wRKjHu10uTMz4VLZNAmVdobpHA8x-KrKw_Wjp0K8" />
+            </NavLink>
+          </nav>
+        );
+    }
+}
+
+/**
+ * CONTAINER
+ */
+const mapStateToProps = state => {
+    return {
+      allCenters: state.centers
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      loadAllCenters: () => dispatch(fetchCentersThunk()),
+    };
+  };
+
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Navbar);
