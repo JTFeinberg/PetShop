d14df27fd9ad52004c2292ec2803d9dfe0638146
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-16 10:49:04 -0400

    Add Navbar component to app

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import {Navbar} from './components'
 import Routes from './routes'
+
 
 
 const App = () => {
   return (
     <div>
-      <nav>
-        Welcome!
-      </nav>
+      <Navbar />
       <main>
        <Routes />
       </main>
