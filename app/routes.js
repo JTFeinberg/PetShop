0bd78d3e25d1c77c1a6054393fdcd9a4c3be5f2f
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-15 21:10:31 -0400

    Add index to components and Add Link to LandingPage for All Centers

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import LandingPage from './components/LandingPage';
-import AllCenters from './components/AllCenters';
+import {LandingPage, AllCenters} from './components';
 
 const Routes = () => (
     <Switch>
-        <Route exact path ="/" component={LandingPage}/>
+        <Route exact path ="/" component={LandingPage} />
         <Route exact path="/centers" component={AllCenters} />
     </Switch>
 )
