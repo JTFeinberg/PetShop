34b1a55b44c5e5df417acd7c446364b1cfa9aa21
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-16 21:53:17 -0400

    Add welcome text to carousel

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -1,55 +1,44 @@
 "use strict";
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class Carousel extends Component {
-    constructor() {
-        super()
-        this.state = {
-            imgIdx: 0,
-            imgUrls: [
-                'https://www.nycacc.org/themes/acc/images/slides/hero_home_1.jpg',
-                'https://www.nycacc.org/themes/acc/images/slides/hero_home_2.jpg',
-                'https://www.nycacc.org/themes/acc/images/slides/hero_home_3.jpg',
-                'https://www.nycacc.org/themes/acc/images/slides/hero_home_4.jpg',
-    
-            ]
-        };
-    }
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.setState({imgIdx: this.state.imgIdx + 1});
-        }, 5000);
-    }
-
-    componentWillUnmount(){
-        clearInterval(this.intervalId);
-      }
-
-    render () {
-        let {imgIdx, imgUrls} = this.state;
-      return (
-        <div id="carousel">
-          {imgUrls.map((url, idx) => {
-              let imgClass = imgIdx % imgUrls.length === idx ? 'carousel-image' : 'carousel-image hidden';
-              return (<img className={imgClass} src={url} key={idx} />);
-          })}
-        </div>
-      );
-    }
-  } 
-
-
-  const ImageSlide = ({ url }) => {
-    const styles = {
-      background: `url(${url})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      height: '420px',
-      width: '100vw'
+  constructor() {
+    super();
+    this.state = {
+      imgIdx: 0,
+      imgUrls: [
+        "https://www.nycacc.org/themes/acc/images/slides/hero_home_1.jpg",
+        "https://www.nycacc.org/themes/acc/images/slides/hero_home_2.jpg",
+        "https://www.nycacc.org/themes/acc/images/slides/hero_home_3.jpg",
+        "https://www.nycacc.org/themes/acc/images/slides/hero_home_4.jpg"
+      ]
     };
-  
-    return (
-      <div className="image-slide" style={styles}>YO</div>
-    );
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({ imgIdx: this.state.imgIdx + 1 });
+    }, 5000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    let { imgIdx, imgUrls } = this.state;
+    return (
+      <section id="carousel">
+        <div id="carousel-text">
+            <h1>Welcome to the NYC Pet Adoption Center!</h1>
+        </div>
+        {imgUrls.map((url, idx) => {
+          let imgClass =
+            imgIdx % imgUrls.length === idx
+              ? 'carousel-image'
+              : 'carousel-image hidden';
+          return <img className={imgClass} src={url} key={idx} />;
+        })}
+      </section>
+    );
+  }
+}
