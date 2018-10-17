0cc26e400d10653ab200f283c43c8e43d9406602
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-16 20:03:31 -0400

    Create ImageSlide component and put in Carousel

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -4,8 +4,23 @@ import React, {Component} from 'react'
 export default class Carousel extends Component {
     render () {
       return (
-        <div className="carousel"></div>
+        <div className="carousel">
+          <ImageSlide url={ imgUrl } />
+        </div>
       );
     }
+  }
+
+
+  const ImageSlide = ({ url }) => {
+    const styles = {
+      backgroundImage: `url(${url})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
+    };
+  
+    return (
+      <div className="image-slide" style={styles}></div>
+    );
   }
 
