853efc607f7631c3fcde7aefb1019c7afb3dc7c5
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-18 11:16:42 -0400

    Add placeholder text to landing page

--- app/components/Card.js
+++ app/components/Card.js
@@ -3,25 +3,27 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 
-const Card = ({center}) => (
+const Card = ({center, selection}) => {
+    let data = center || selection;
+    return (
     <li className='card-body columns'>
         <div>
             <span>
-                <img src={center.imageUrl} style={{ width: "100%", height: "345px"}} />
+                <img src={data.imageUrl} style={{ width: "100%", height: "345px"}} />
             </span>
         </div>
         <div>
             <span>
-                <h4>{center.name}</h4>
+                <h4>{data.name}</h4>
             </span>
         </div>
         <div>
             <span>
-                <h6>{center.location}</h6>
+                <h6>{data.location}</h6>
             </span>
         </div>
     </li>
-);
+)};
 
 
 export default Card;
--- app/components/LandingPage.js
+++ app/components/LandingPage.js
@@ -3,38 +3,39 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchCentersThunk} from '../store'
 import {Link} from 'react-router-dom'
+//import Card from './card'
 
 
-class LandingPage extends Component {
-    componentDidMount() {
-        this.props.loadAllCenters()
-    }
-
-    render() {
-        return (
+const LandingPage = () => 
+    // const selections = [
+    //   {type: 'Locations',
+    //   imgUrl:},
+    //   {type:'All Adoptions',
+    //     imgUrl:},
+    //     {type: 'Dogs',
+    //       imgUrl:},
+    //       {type: 'Cats',
+    //         imgUrl:},
+    //         {type: 'Bunnies',
+    //           imgUrl:}
+    // ]
+        (
             <div>
+              <p>
+                <span style={{fontSize: "2.25vw", fontFamily: "Arial", lineHeight: "2"}}>
+                Surrendering your cat or bringing in a stray cat?
+                <br />
+                WAIT!
+                <br />
+                ACC is over capacity with cats! We are temporarily asking the public to schedule all <a href="http://nycacc.org/services/surrender"> pet surrenders</a>, so that we can better attend to the animals currently in our care centers.
+                <br />
+                Found a stray cat? Bringing in a stray cat may not be in his/her best interest at this time if the cat appears healthy and thriving in the community. If the cat appears injured, please call 311.
+                </span>
+              </p>
                  <Link to="/centers">See All Centers</Link>
             </div>
-        )
-    }
-}
+        );
 
-/**
- * CONTAINER
- */
-const mapStateToProps = state => {
-    return {
-      allCenters: state.centers
-    };
-  };
-  
-  const mapDispatchToProps = dispatch => {
-    return {
-      loadAllCenters: () => dispatch(fetchCentersThunk()),
-    };
-  };
 
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(LandingPage);
+
+  export default LandingPage;
