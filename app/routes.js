8a247bf326ea007206d410c082b17ab27fb17492
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-24 12:15:22 -0400

    Connect singleAnimal component to the app

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import  {AllCenters, LandingPage, AllAnimals} from './components';
+import  {AllCenters, LandingPage, AllAnimals, SingleAnimal} from './components';
 
 const Routes = () => (
     <Switch>
         <Route exact path ="/" component={LandingPage} />
         <Route exact path="/Locations" component={AllCenters} />
         <Route exact path="/All Adoptions" component={AllAnimals} />
+        <Route exact path="/All Adoptions/:animalId" component={SingleAnimal} />
     </Switch>
 )
 
