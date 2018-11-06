d58efb81d235aa78596ac15ad4d61458898141fc
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-05 20:56:50 -0500

    Put filter criteria on state

--- app/components/AllAnimals.js
+++ app/components/AllAnimals.js
@@ -11,10 +11,41 @@ class AllAnimals extends Component {
     this.state = {
       id: '',
       centerId: '',
-      type: ''
+      type: '',
+      filterCriteria: {},
+      animalTypes: [
+        {id: 1, name: 'All Adoptions'},
+        {id: 2, name: 'Dog'},
+        {id: 3, name: 'Cat'},
+        {id: 4, name: 'Rabbit'}
+      ]
     };
 
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    /*
+    location.search = ?centerId=${id#}&type=${type}&id=${id#}
+    type refers to the type of animal (Dog,Cat, Rabbit)
+    id refers to the animalId
+    */
+    if (this.props.location.search.length) {
+      let filterCriteria = this.props.location.search.slice(1).split('&').map(serachString => {
+        let searchArr = serachString.split("=")
+        this.setState({[searchArr[0]]: searchArr[1]})
+        return searchArr;
+      })
+      //Otherwise, return an obj with filter types as keys and the filter criteria as the values
+      .reduce((filterObj, serachArr) => {
+        if (serachArr[1].length) {
+          filterObj[serachArr[0]] = serachArr[1];
+        }
+        return filterObj;
+      }, {});
+      this.setState({filterCriteria});
+
+    }
   }
 
   handleChange({ target }) {
@@ -23,26 +54,9 @@ class AllAnimals extends Component {
   }
 
   render() {
-    let { allAnimals, loadSingleAnimal, location, centers } = this.props;
-    let { id, centerId, type } = this.state;
-    /*
-    location.search = ?centerId=${id#}&type=${type}&id=${id#}
-    type refers to the type of animal (Dog,Cat, Rabbit)
-    id refers to the animalId
-    */
-  
-    //if no filters applied then simply return an empty obj. Otherwise, return an obj with filter types as keys and the filter criteria as the values
-    let filterCriteria = location.search.length ? location.search
-      .slice(1)
-      .split("&")
-      .map(serachString => serachString.split("="))
-      .reduce((filterObj, serachArr) => {
-        if (serachArr[1].length) {
-          filterObj[serachArr[0]] = serachArr[1];
-        }
-        return filterObj;
-      }, {}) : {};
-  
+    let { allAnimals, loadSingleAnimal, centers } = this.props;
+    let { id, centerId, animalTypes, type, filterCriteria } = this.state;
+    
     //Use the filterCriteria obj to filter allAnimals. This variable will be used in the returned JSX
     let filteredAnimals = allAnimals.filter(animal => {
       for (let filter in filterCriteria) {
@@ -52,12 +66,6 @@ class AllAnimals extends Component {
       }
       return true;
     });
-    const animalTypes = [
-      {id: 1, name: 'All Adoptions'},
-      {id: 2, name: 'Dog'},
-      {id: 3, name: 'Cat'},
-      {id: 4, name: 'Rabbit'}
-    ];
 
     return (
       <ul className="allAnimals">
