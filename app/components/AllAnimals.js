3de7daa2f38f42502f207f6c59cedc8b796cd211
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-06 18:30:34 -0500

    Add border, height, and overall sizing for form

--- app/components/AllAnimals.js
+++ app/components/AllAnimals.js
@@ -88,7 +88,7 @@ class AllAnimals extends Component {
           <option key={center.id} value={center.id}>{center.name}</option>
           ))}
         </select>
-        <input type="submit" value="Search" />
+        <input className="filter-submit" type="submit" value="Search" />
         </form>
         <div className="row">
           {filteredAnimals.map(animal => (
